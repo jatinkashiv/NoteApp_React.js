b5800abfbe80f9761d10722db189bffcfd49a4ac
Jatin Kashiv <jatinkashiv2003@gmail.com>
2025-06-27 22:46:39 +0530

    integrate all components into App

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,6 +3,14 @@ import AddNote from "./components/AddNote";
 import NoteList from "./components/NoteLists";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="app-container">
+      <h1>Notes App</h1>
+      <AddNote />
+      <ul>
+        <NoteList />
+      </ul>
+    </div>
+  );
 }
 export default App;
